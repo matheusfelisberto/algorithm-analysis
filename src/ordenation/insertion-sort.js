65017b966484aa2eb399993e4afeb3b96e4d1742
Matheus Felisberto <matheus.felisberto@hotmail.com>
2018-07-13 01:38:41 -0300

    add(ordenation) insertion sort implementation

---
+++ src/ordenation/insertion-sort.js
@@ -0,0 +1,25 @@
+/**
+ * Insertion sort ordenation algoritm
+ * Time Complexity: O(n*n)
+ * 
+ * @see https://www.geeksforgeeks.org/insertion-sort/
+ * @param {Array} data
+ * @return {Array}
+ */
+module.exports = data => {
+  let temporary, j;
+
+  for (let i = 0; i < data.length; i++) {
+    j = i;
+    temporary = data[i];
+
+    while (j > 0 && data[j - 1] > temporary) {
+      data[j] = data[j - 1];
+      j--;
+    }
+
+    data[j] = temporary;
+  }
+
+  return data;
+};
